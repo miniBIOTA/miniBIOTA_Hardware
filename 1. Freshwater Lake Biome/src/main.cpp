62604e85fd8b811dee94133f94fd95075a180f9a
miniBIOTA <josue@minibiota.com>
2026-04-24 13:14:28 -0400

    Add OTA functionality and configuration for Freshwater Lake Biome

--- 1. Freshwater Lake Biome/src/main.cpp
+++ 1. Freshwater Lake Biome/src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ArduinoOTA.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* WIFI_SSID = "mB2.4";
+const char* WIFI_PASS = "qYKEQe8R763HKmk";
+const char* OTA_HOSTNAME = "biome1-lake";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(500);
+
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("Connected. IP: ");
+  Serial.println(WiFi.localIP());
+
+  ArduinoOTA.setHostname(OTA_HOSTNAME);
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("OTA start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("OTA done");
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("OTA error [%u]\n", error);
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA ready.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  ArduinoOTA.handle();
 }
